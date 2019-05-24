075c90ee7a52308b87c7baa88db44c0de0aff1e7
Sarah Seif El Nasr-Storey <Sarah@storeymcpr.dyndns.cern.ch>
2019-05-24 19:17:27 +0200

    Modified notebook to use LoadData.py

---
+++ LoadData.py
@@ -0,0 +1,22 @@
+import sys
+import pandas as pd
+
+def loadmsoaData(dirName=''):
+	msoaData = pd.read_excel('%s/msoa_loneliness.xlsx'%(dirName), 'msoa_loneliness', index_col=None)
+	msoaDataDict =  pd.read_excel('%s/msoa_loneliness.xlsx'%(dirName), 'Data Dictionary', index_col=None)
+	return msoaData, msoaDataDict
+
+def loadFinalData(dirName=''):
+	finalData = pd.read_excel('%s/final_data.xlsx'%(dirName), 'Data', index_col=None)
+	finalDataDict =  pd.read_excel('%s/final_data.xlsx'%(dirName), 'Data Dictionary', index_col=None)
+	return finalData, finalDataDict
+
+def loadDrugsList(dirName=''):
+	drugsList = pd.read_csv('%s/drug_list.csv'%(dirName))
+	return drugsList
+
+def loadProccessedData(dirName=''):
+	processedData = pd.read_csv('%s/processed_data.csv'%(dirName))
+	# drop the un-named column
+	processedData = processedData.drop(['Unnamed: 0'], axis=1)
+	return processedData
